6b16cc81e6d74315b87369dc13dd2bf1042e0db9
TrushitMarviya <marviyatrushit0@gmail.com>
2024-03-21 15:37:59 +0530

    pattern-1

---
+++ Pattern-1/Q4.c
@@ -0,0 +1,17 @@
+// 1 2 3 4 5
+// 2 3 4 5
+// 3 4 5
+// 4 5
+// 5
+
+#include<stdio.h>
+main(){
+    for (int i = 1; i <=5; i++)
+    {
+        for (int j=i+1 ;j<=5;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+}
---
+++ Pattern-1/Q6.c
@@ -0,0 +1,18 @@
+// 1 0 1 0 1
+// 1 0 1 0
+// 1 0 1
+// 1 0 
+// 1
+
+
+#include<stdio.h>
+main(){
+    for (int i = 1; i <=5; i++)
+    {
+        for (int j=5;j>=i;j--)
+        {
+            printf("%d",j%2);
+        }
+        printf("\n");
+    }
+}
