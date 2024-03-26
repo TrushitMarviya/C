bac6ae6ee0d278ee2c6a8ce21708b767038346d3
TrushitMarviya <marviyatrushit0@gmail.com>
2024-03-26 14:44:07 +0530

    pr-4

---
+++ Circlet/Q4.c
@@ -0,0 +1,26 @@
+// 1                 1
+// 1 2             2 1
+// 1 2 3         3 2 1
+// 1 2 3 4     4 3 2 1
+// 1 2 3 4 5 5 4 3 2 1
+
+#include<stdio.h>
+main(){
+   for (int i = 1; i <=5; i++)
+   {
+        for (int j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        for (int k=8;k>=i*2-1;k--)
+        {
+            printf("_");
+        }
+        for (int s=i;s>=1;s--)
+        {
+            printf("%d",s);
+        }
+                
+        printf("\n");
+   }
+}
