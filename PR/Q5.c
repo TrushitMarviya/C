c55e87d9a3c2b0d03e9e09e1c9d75c07022765ea
TrushitMarviya <marviyatrushit0@gmail.com>
2024-03-28 15:34:51 +0530

    pr

---
+++ PR/Q5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main(){
+    for (int i =4; i>=1; i--)
+    {
+        for (int s=4;s>=i;s--)
+        {
+            printf(" ");
+        }
+        for (int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+        for (int i =2; i<=4; i++)
+    {
+        for (int s=4;s>=i;s--)
+        {
+            printf(" ");
+        }
+        for (int j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+}
+}
