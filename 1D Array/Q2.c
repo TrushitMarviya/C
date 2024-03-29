5e0d4620212cd2a56b2b6d796246a77cf27ef209
TrushitMarviya <marviyatrushit0@gmail.com>
2024-03-29 15:52:57 +0530

    array

---
+++ 1D Array/Q2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main(){
+   int arr[5],sum=0;
+
+for (int i = 0; i<5; i++)
+{
+    printf("Enter Element:");
+    scanf("%d",&arr[i]);
+}
+for (int i = 0; i <5; i++)
+{       sum+=arr[i];
+}
+    printf("Sum is:%d\n",sum);
+
+}
---
+++ 1D Array/Q8.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main(){
+   int arr[5];
+for (int i = 0; i<5; i++)
+{
+    printf("Enter Element:");
+    scanf("%d",&arr[i]);
+}
+for (int i =4; i >=0; i--)
+{       
+    printf("%d\n",arr[i]);
+}
+}
