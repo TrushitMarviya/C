8c31ceabadb2fef30ffe10c3164f5c7115782055
TrushitMarviya <marviyatrushit0@gmail.com>
2024-04-19 12:56:48 +0530

    project2

--- Superior/Q8.c
+++ Superior/Q8.c
@@ -6,7 +6,7 @@ int main()
     scanf("%d", &size1);
     printf("Enter size of Second  array : ");
     scanf("%d", &size2);
-    int arr1[size1],arr2[size2],merg[size1 + size2];
+    int arr1[size1],arr2[size2];
     for (int i = 0; i < size1; i++)
     {
         printf("Enter  Elements of First array :");
@@ -17,21 +17,16 @@ int main()
         printf("Enter Elements of Second array :");
         scanf("%d", &arr2[j]);
     }
-    for (int x = 0; x < size1 + size2; x++)
+    for (int i = 0; i < size1; i++)
     {
-        if (x < size1)
+        for (int j = 0; j < size2; j++)
         {
-            merg[x] = arr1[x];
-        }
-        else
-        {
-            merg[x] = arr2[x - size1];
-        }
+            if (arr1[i] == arr2[j])
+            {
+                printf("Intersection Elements are : %d \n", arr1[i]);
+            }
+        }  
     }
-    for (int x = 0; x < size1 + size2; x++)
-    {
-        printf("%d ", merg[x]);
-    }
-
+    
     return 0;
 }
